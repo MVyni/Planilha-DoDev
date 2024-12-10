acf95004bdbc0ed7f86b43e904fe9ae0b2ae5e9a
Marcus Vynicius <viniciusfla1999@gmail.com>
2024-12-10 23:00:45 +0000

    criando uma API e criando uma função POST com os intens da planilha nessa API

--- index.js
+++ index.js
@@ -28,4 +28,20 @@ async function ReadWorkSheet() {
     return list;
 }
 
-ReadWorkSheet();
+async function AddUser(data= {}){
+    const response = await fetch("https://api-generator.retool.com/ddYO0f/Usuário", {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json"},
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(data)
+        
+    });
+    return response.json();
+}
+
+AddUser(ReadWorkSheet());
